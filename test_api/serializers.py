a1c0a0fff25a2a8bca206383094638ac5709a064
Viktoriya Diukina <viki_dyukina@mail.ru>
2024-11-12 23:50:29 +0400

    deleted testing app

--- test_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from notes_app.models import Notes
-
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notes
-        fields = '__all__'
